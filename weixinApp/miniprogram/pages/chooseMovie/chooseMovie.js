d34c9982951676f150eda12a3097c07e48b90ca1
aocrin <46671250+aocrin@users.noreply.github.com>
2019-01-16 17:03:29 +0800

    影院

---
+++ weixinApp/miniprogram/pages/chooseMovie/chooseMovie.js
@@ -0,0 +1,68 @@
+// pages/chooseMovie/chooseMovie.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    storeName:'苏州中心CGV影院',
+    movieAddress:'江苏省苏州中心4楼',
+    movieTel:'0512-80911313',
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
