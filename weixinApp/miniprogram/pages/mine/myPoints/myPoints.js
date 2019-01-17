1725b60938caac186a4032cedfbad81727afcc57
dhc-zouke <37171769+dhc-zouke@users.noreply.github.com>
2019-01-17 17:05:35 +0800

    myPoints 下拉列表 20190117
    myPoints 下拉列表 20190117

--- weixinApp/miniprogram/pages/mine/myPoints/myPoints.js
+++ weixinApp/miniprogram/pages/mine/myPoints/myPoints.js
@@ -8,65 +8,83 @@ Page({
     state: false,
     first_click: false,
     select: '',
-    tipsshow: 'block'
+    tipsshow: 'block',
+    show: false,
+    selectData: ['1', '2', '3', '4', '5', '6'], //下拉列表的数据
+    index: 0 //选择的下拉列表下标
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   },
-  toggle: function () {
+
+  selectTap: function() {
+    this.setData({
+      show: !this.data.show
+    });
+  },
+
+  optionTap: function() {
+    let Index = e.currentTarget.dataset.index; //获取点击的下拉列表的下标
+    this.setData({
+      index: Index,
+      show: !this.data.show
+    });
+  },
+
+  toggle: function() {
     var list_state = this.data.state,
       first_state = this.data.first_click;
     if (!first_state) {
