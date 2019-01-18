03ac28aa9dd4bb8b2dd052baf28eb99ef56f8dd5
vannewang <46670766+vannewang@users.noreply.github.com>
2019-01-18 17:10:02 +0800

    优惠券抽模板

--- weixinApp/miniprogram/pages/mine/coupon/coupon.js
+++ weixinApp/miniprogram/pages/mine/coupon/coupon.js
@@ -5,62 +5,88 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    couponList: [{
+        'title': '暖心季品质联盟红包',
+        'time': '2019-1-17至2019-1-24',
+        'msg_one': '仅限于品质联盟会场使用',
+        'msg_two': '满30元可用',
+        'msg_three': '限在线支付可用',
+        'price': '2.5',
+        'use': '使用'
+      },
+      {
+        'title': '暖心季品质联盟红包',
+        'time': '2019-1-12至2019-1-29',
+        'msg_one': '仅限于品质联盟会场使用',
+        'msg_two': '满35元可用',
+        'msg_three': '限在线支付可用',
+        'price': '4.1',
+        'use': '使用'
+      },
+      {
+        'title': '暖心季品质联盟红包',
+        'time': '2019-1-12至2019-1-29',
+        'msg_one': '仅限于品质联盟会场使用',
+        'msg_two': '满41元可用',
+        'msg_three': '限在线支付可用',
+        'price': '5.5',
+        'use': '使用'
+      }
+    ]
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
