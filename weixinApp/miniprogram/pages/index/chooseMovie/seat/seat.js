abdebf569d437c9030984ae2cdbcc10e231425a2
aocrin <46671250+aocrin@users.noreply.github.com>
2019-01-21 17:39:39 +0800

    选座

--- weixinApp/miniprogram/pages/index/chooseMovie/seat/seat.js
+++ weixinApp/miniprogram/pages/index/chooseMovie/seat/seat.js
@@ -1,11 +1,25 @@
 // pages/index/chooseMovie/seat/seat.js
+var limt = 0
+function formatNumber(n) {
+  n = n.toString()
+  return n[1] ? n : '0' + n
+}
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    shop: { shop: 'CGV影城(苏州中心店)', time: '今天 1月21号 16:25', effects: '英语3D', room: '3DIMAX厅', center: 6, price: 36.2,},
+    map: [],
+    deltaX:30,
+    deltaY:1,
+    seats:[],
+    willChange: false,
+    hasSelected: false,
+    seatmap: [[1, 0, 2, 2, 2, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1], [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1],
+      [1, 0, 1, 1, 1, 1, 1, 1, 2, 2, 1, 0, 0, 1, 1, 1, 1], [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1],
+      [1, 0, 1, 1, 1, 2, 2, 2, 2, 1, 1, 0, 0, 1, 1, 1, 1]]
   },
 
   /**
@@ -19,7 +33,14 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    var columnArr = []
+    var map = this.data.seatmap
+    for (var i = 1; i <= map.length; i++) {
+      columnArr.push(i)
+    }
+    this.setData({
+      columnArr: columnArr
+    })
   },
 
   /**
@@ -62,5 +83,86 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  scrollstart: function (ev) {
+    this.sX = ev.changedTouches[0].clientX
+    this.sY = ev.changedTouches[0].clientY
+    this.setData({
+      willChange: true
+    })
+    console.log(ev)
+  },
+  scrollmove: function (ev) {
+    var mX = ev.changedTouches[0].clientX
+    var mY = ev.changedTouches[0].clientY
+    var deltaX = (mX - this.sX) / 2
+    var deltaY = (mY - this.sY) / 2
+    this.setData({
+      deltaX: deltaX,
+      deltaY: deltaY
+    })
+  },
+  scrollend: function (ev) {
+    var eX = ev.changedTouches[0].clientX
+    var eY = ev.changedTouches[0].clientY
+    console.log(ev)
+    this.setData({
+      willChange: false
+    })
+  },
+  selectSeat: function (ev) {
+    var ver = ev.currentTarget.dataset.ver
+    var hor = ev.currentTarget.dataset.hor
+    var map = this.data.seatmap
+    var seats = []
+    var cStr = ''
+    limt++
+    if (limt <= 4) {
+      map[ver][hor] = 3
+      for (var i = 0; i < map.length; i++) {
+        for (var j = 0; j < map[i].length; j++) {
+          if (map[i][j] === 3) {
+            cStr = formatNumber(i + 1) + '排' + (j + 1) + '座'
+            seats.push(cStr)
+          }
+        }
+      }
+      this.setData({
+        seatmap: map,
+        seats: seats
+      })
+    }
+    else {
+      wx.showToast({
+        title: '最多选4个座位',
+        icon: 'success',
+        duration: 2000
+      })
+      limt = 4
+    }
+  },
+  cancelSeat: function (ev) {
+    var ver = ev.currentTarget.dataset.ver
+    var hor = ev.currentTarget.dataset.hor
+    var cStr = ''
+    var seats = []
+    console.log(ev)
+    var map = this.data.seatmap
+    limt--
+    map[ver][hor] = 1
+    for (var i = 0; i < map.length; i++) {
+      for (var j = 0; j < map[i].length; j++) {
+        if (map[i][j] === 3) {
+          cStr = formatNumber(i + 1) + '排' + (j + 1) + '座'
+          seats.push(cStr)
+        }
+      }
+    }
+    this.setData({
+      seatmap: map,
+      seats: seats
+    })
   }
+
 })
