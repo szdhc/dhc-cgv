fa641bab6110965fb905de13347848709c0ae391
xiexiaobai <38690764+xiexiaobai@users.noreply.github.com>
2019-01-21 11:28:35 +0800

    首页修改 20190121
    首页修改 20190121

--- weixinApp/miniprogram/pages/index/filmDetails/filmDetails.js
+++ weixinApp/miniprogram/pages/index/filmDetails/filmDetails.js
@@ -1,66 +1,93 @@
-// miniprogram/pages/index/filmDetails/filmDetails.js
+// pages/movieInfo/movieInfo.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    filmDetails_info: {},
+    filmType: '',
+    wantFlag: false,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    let Id = options.id;
 
+    // 开启导航条加载动画 
+    wx.showNavigationBarLoading();
+    wx.request({
+      url: 'https://douban.uieee.com/v2/movie/subject/' + Id,
+      method: "GET",
+      header: {
+        'Content-Type': 'json',
+      },
+      success: (({ data }) => {
+        this.setData({
+          filmDetails_info: data,
+          filmType: this.options.type
+        })
+        console.log(data)
+
+        //动态设置电影详情setNavigationBarTitle
+        wx.setNavigationBarTitle({
+          title: this.data.filmDetails_info.title,
+        })
+
+        //数据回来之后隐藏加载动画
+        wx.hideNavigationBarLoading();
+        //请求成功同步数据
+        // console.log(this.data.moviesData)
+      })
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+   //   点击想看
+    clickWant(e) {
+    this.setData({
+      wantFlag: !this.data.wantFlag
+    })
+    // wantFilms
+    if (this.data.wantFlag) {
+      wx.request({
+        url: this.data.IP + '/users/update',
+        data: {
+          _id: wx.getStorageSync("userId"),
+          film: e.currentTarget.id,
+          isPush: true
+        },
+        success: ((res) => {
+        })
+      })
+      wx.showToast({
+        title: '已标记想看',
+        icon: 'success',
+        duration: 1500
+      })
+    } else {
+      wx.request({
+        url: this.data.IP + '/users/find',
+        data: {
+          _id: wx.getStorageSync("userId")
+        },
+        success: ((res) => {
+          wx.request({
+            url: this.data.IP + '/users/update',
+            data: {
+              _id: wx.getStorageSync("userId"),
+              film: res.data.film.filter(item => item != e.currentTarget.id)
+            },
+            success: ((res) => {
+            })
+          })
+        })
+      })
+      wx.showToast({
+        title: '已取消想看',
+        icon: 'success',
+        duration: 1500
+      })
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
